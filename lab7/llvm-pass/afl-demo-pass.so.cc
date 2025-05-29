ac6f4621309264c2a8f713097ce886de07399341
Zryxion <90599845+Zryxion@users.noreply.github.com>
2025-05-29 10:36:25 +0800

    Update afl-demo-pass.so.cc

--- lab7/llvm-pass/afl-demo-pass.so.cc
+++ lab7/llvm-pass/afl-demo-pass.so.cc
@@ -1,67 +1,69 @@
 #define AFL_LLVM_PASS
 
-#include "llvm/IR/PassManager.h"
+#include "config.h"
+#include "debug.h"
+
+#include <string>
 #include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/Instructions.h"
+#include "llvm/IR/PassManager.h"
 #include "llvm/Passes/PassBuilder.h"
-#include "llvm/Passes/PassPlugin.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/Passes/OptimizationLevel.h"
+#include "llvm/IR/InstrTypes.h"
+#include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
 
 namespace {
 
 class AFLDEMOPass : public PassInfoMixin<AFLDEMOPass> {
-public:
-  PreservedAnalyses run(Module &M, ModuleAnalysisManager &) {
-    LLVMContext &C = M.getContext();
-    Type *VoidTy = Type::getVoidTy(C);
-    FunctionCallee demo_crash = M.getOrInsertFunction("__demo_crash", VoidTy);
 
-    for (Function &F : M) {
-      for (BasicBlock &BB : F) {
-        for (Instruction &I : BB) {
-          if (auto *CI = dyn_cast<CallInst>(&I)) {
-            Function *calledFunc = CI->getCalledFunction();
-            if (!calledFunc) continue;
-            if (calledFunc->getName() == "system") {
-              Value *arg = CI->getArgOperand(0);
-              
-              // Strip any pointer casts
-              arg = arg->stripPointerCasts();
-            
-              // Skip instrumentation for direct constant global strings
-              if (isa<GlobalVariable>(arg)) {
-                GlobalVariable *GV = cast<GlobalVariable>(arg);
-                if (GV->hasInitializer()) {
-                  if (isa<ConstantDataArray>(GV->getInitializer())) {
-                    continue; // skip constant system("echo AAA")
-                  }
-                }
-              }
-            
-              // If it's not a constant string, treat it as potential injection
-              IRBuilder<> IRB(CI);
-              IRB.CreateCall(demo_crash);
+ public:
+  PreservedAnalyses run(Module &M, ModuleAnalysisManager &);
+
+};
+
+}  // namespace
+
+extern "C" ::llvm::PassPluginLibraryInfo LLVM_ATTRIBUTE_WEAK
+llvmGetPassPluginInfo() {
+  return {LLVM_PLUGIN_API_VERSION, "AFLDEMOPass", "v0.1",
+          [](PassBuilder &PB) {
+            PB.registerOptimizerLastEPCallback(
+                [](ModulePassManager &MPM, OptimizationLevel) {
+                  MPM.addPass(AFLDEMOPass());
+                });
+          }};
+}
+
+PreservedAnalyses AFLDEMOPass::run(Module &M, ModuleAnalysisManager &) {
+
+  LLVMContext &C = M.getContext();
+  Type *VoidTy = Type::getVoidTy(C);
+  FunctionCallee demo_crash = M.getOrInsertFunction("__demo_crash", VoidTy);
+
+  for (auto &F : M) {
+    for (auto &BB : F) {
+      for (auto &I : BB) {
+        if (auto *call = dyn_cast<CallInst>(&I)) {
+          Function *callee = call->getCalledFunction();
+          if (!callee) continue;
+
+          if (callee->getName() == "system") {
+            Value *arg = call->getArgOperand(0);
+
+            if (!isa<ConstantDataArray>(arg->stripPointerCasts())) {
+              // Likely attacker-controlled string, inject crash
+              IRBuilder<> IRB(call);
+              IRB.CreateCall(demo_crash)->setMetadata(M.getMDKindID("nosanitize"),
+                                                       MDNode::get(C, {}));
             }
           }
         }
       }
     }
-    return PreservedAnalyses::none();
   }
-};
 
-} // namespace
-
-extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo llvmGetPassPluginInfo() {
-  return {
-    LLVM_PLUGIN_API_VERSION, "AFLDEMOPass", "v0.1",
-    [](PassBuilder &PB) {
-      PB.registerPipelineParsingCallback(
-        [](StringRef, ModulePassManager &MPM, ArrayRef<PassBuilder::PipelineElement>) {
-          MPM.addPass(AFLDEMOPass());
-          return true;
-        });
-    }
-  };
+  return PreservedAnalyses::all();
 }
