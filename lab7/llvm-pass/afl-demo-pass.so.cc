7cd22fa8c4f506d4753b430658cc2e73c29ac892
Zryxion <90599845+Zryxion@users.noreply.github.com>
2025-05-29 09:09:38 +0800

    Update afl-demo-pass.so.cc

--- lab7/llvm-pass/afl-demo-pass.so.cc
+++ lab7/llvm-pass/afl-demo-pass.so.cc
@@ -3,70 +3,67 @@
 #include "config.h"
 #include "debug.h"
 
-#include <llvm/IR/IRBuilder.h>
-#include <llvm/IR/Instructions.h>
-#include <llvm/IR/PassManager.h>
-#include <llvm/Passes/PassPlugin.h>
-#include <llvm/Passes/PassBuilder.h>
-#include <llvm/IR/Module.h>
-#include <llvm/Support/raw_ostream.h>
+#include <string>
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/PassManager.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/Passes/OptimizationLevel.h"
+#include "llvm/IR/InstrTypes.h"
+#include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
 
 namespace {
 
 class AFLDEMOPass : public PassInfoMixin<AFLDEMOPass> {
+
  public:
-  PreservedAnalyses run(Module &M, ModuleAnalysisManager &) {
-    LLVMContext &C = M.getContext();
-    Type *VoidTy = Type::getVoidTy(C);
-    FunctionCallee demo_crash = M.getOrInsertFunction("__demo_crash", VoidTy);
+  PreservedAnalyses run(Module &M, ModuleAnalysisManager &);
 
-    for (Function &F : M) {
-      if (F.isDeclaration()) continue;
+};
 
-      StringRef fn = F.getName();
-      if (fn.startswith("_start") || fn.startswith("__libc_csu") ||
-          fn.startswith("__afl_") || fn.startswith("__asan") ||
-          fn.startswith("asan.") || fn.startswith("llvm."))
-        continue;
+}  // namespace
 
-      for (BasicBlock &BB : F) {
-        for (Instruction &I : BB) {
-          if (auto *call = dyn_cast<CallInst>(&I)) {
-            Function *calledFunc = call->getCalledFunction();
-            if (!calledFunc) continue;
+extern "C" ::llvm::PassPluginLibraryInfo LLVM_ATTRIBUTE_WEAK
+llvmGetPassPluginInfo() {
+  return {LLVM_PLUGIN_API_VERSION, "AFLDEMOPass", "v0.1",
+          [](PassBuilder &PB) {
+            PB.registerOptimizerLastEPCallback(
+                [](ModulePassManager &MPM, OptimizationLevel) {
+                  MPM.addPass(AFLDEMOPass());
+                });
+          }};
+}
 
-            if (calledFunc->getName() == "system") {
-              // Only consider non-constant arguments (possible user input)
-              Value *arg = call->getArgOperand(0);
-              if (!isa<Constant>(arg)) {
-                IRBuilder<> IRB(call);
-                IRB.CreateCall(demo_crash)->setMetadata(
-                    M.getMDKindID("nosanitize"), MDNode::get(C, None));
-              }
+PreservedAnalyses AFLDEMOPass::run(Module &M, ModuleAnalysisManager &) {
+
+  LLVMContext &C = M.getContext();
+  Type *VoidTy = Type::getVoidTy(C);
+  FunctionCallee demo_crash = M.getOrInsertFunction("__demo_crash", VoidTy);
+
+  for (auto &F : M) {
+    for (auto &BB : F) {
+      for (auto &I : BB) {
+        if (auto *call = dyn_cast<CallInst>(&I)) {
+          Function *callee = call->getCalledFunction();
+          if (!callee) continue;
+
+          if (callee->getName() == "system") {
+            Value *arg = call->getArgOperand(0);
+
+            if (!isa<ConstantDataArray>(arg->stripPointerCasts())) {
+              // Likely attacker-controlled string, inject crash
+              IRBuilder<> IRB(call);
+              IRB.CreateCall(demo_crash)->setMetadata(M.getMDKindID("nosanitize"),
+                                                       MDNode::get(C, {}));
             }
           }
         }
       }
     }
-
-    return PreservedAnalyses::all();
   }
-};
 
-}  // namespace
-
-extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo llvmGetPassPluginInfo() {
-  return {
-    LLVM_PLUGIN_API_VERSION, "AFLDEMOPass", "v0.2",
-    [](PassBuilder &PB) {
-      PB.registerPipelineParsingCallback(
-        [](StringRef, ModulePassManager &MPM,
-           ArrayRef<PassBuilder::PipelineElement>) {
-          MPM.addPass(AFLDEMOPass());
-          return true;
-        });
-    }
-  };
+  return PreservedAnalyses::all();
 }
